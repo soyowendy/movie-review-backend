bd20d2947850b7346f3d18fbdd6c8b51bb35a6fe
Wenderson Santos <souowendy@gmail.com>
2023-07-17 18:44:19 -0300

    GET All Movies

--- src/main/java/br/com/souowendy/movies/controller/MovieController.java
+++ src/main/java/br/com/souowendy/movies/controller/MovieController.java
@@ -1,5 +1,8 @@
 package br.com.souowendy.movies.controller;
 
+import br.com.souowendy.movies.domain.Movie;
+import br.com.souowendy.movies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All Movies! ", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.AllMovies(), HttpStatus.OK);
     }
 }
--- src/main/java/br/com/souowendy/movies/service/MovieService.java
+++ src/main/java/br/com/souowendy/movies/service/MovieService.java
@@ -1,4 +1,17 @@
 package br.com.souowendy.movies.service;
 
+import br.com.souowendy.movies.domain.Movie;
+import br.com.souowendy.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> AllMovies() {
+        return movieRepository.findAll();
+    }
 }
