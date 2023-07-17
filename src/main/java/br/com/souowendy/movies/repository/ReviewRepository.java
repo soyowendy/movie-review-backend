1cb502e7ca71d1cd8bd69eaae548c2222f2f984d
Wenderson Santos <souowendy@gmail.com>
2023-07-17 19:30:56 -0300

    Post review to a movie

---
+++ src/main/java/br/com/souowendy/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package br.com.souowendy.movies.repository;
+
+import br.com.souowendy.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/main/java/br/com/souowendy/movies/service/ReviewService.java
@@ -0,0 +1,29 @@
+package br.com.souowendy.movies.service;
+
+import br.com.souowendy.movies.domain.Movie;
+import br.com.souowendy.movies.domain.Review;
+import br.com.souowendy.movies.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
